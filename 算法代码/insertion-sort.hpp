6c21791fb845ee7951d85aa4b1d1532685061435
ypeng4641 <yypp1990@hotmail.com>
2016-03-07 00:08:41 +0800

    算法资料上传

---
+++ 算法代码/insertion-sort.hpp
@@ -0,0 +1,13 @@
+/*
+ * ²åÈëÅÅĞòËã·¨
+ * Î±´úÂë
+ */
+for j  2 to A.length
+	key = A[j]
+	//Insert A[j] into the sorted sequence A[1..j-1].
+	i = j-1
+	while i > 0 and A[i] > key
+		A[i+1] = A[i]
+		i = i - 1
+	A[i+1] = key
+
---
+++ 算法代码/merge-sort.hpp
@@ -0,0 +1,32 @@
+/*
+ * πÈ≤¢≈≈–ÚÀ„∑®
+ * Œ±¥˙¬Î
+ * */
+
+MERGE(A, p, q, r)
+n1 = q - p + 1
+n2 = r = q
+//let L[1..n1+1] and R[1..n2+1] be new arrays
+for i = 1 to n1
+	L[i] = A[p+i-1]
+for j  1 to n2
+	R[j] = A[q + j]
+L[n1 + 1] = +oo
+R[n2 + 1] = +oo
+i = 1
+j = 1
+for k = p to r
+	if L[i] <= R[j]
+		A[k] = L[i]
+		i = i + 1
+	else A[k] = R[j]
+		j = j + 1
+
+
+
+MERGE-SORT(A, p, r)
+if p < r
+	q = (p+r)/2
+	MERGE-SORT(A, p, q)
+	MERGE-SORT(a, q+1, r)
+	MERGE(A, p, q, r)
